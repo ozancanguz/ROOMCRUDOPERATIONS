81468959e18df0a5768d037f7d63159fd5d61851
Ozan <ozancangu24@gmail.com>
2022-11-14 10:00:08 +0300

    -User class(entity) coded

--- app/src/main/java/com/ozancanguz/roomcrudoperations/data/User.kt
+++ app/src/main/java/com/ozancanguz/roomcrudoperations/data/User.kt
@@ -1,7 +1,15 @@
 package com.ozancanguz.roomcrudoperations.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity
-class User {
-}
+@Entity(tableName = "user_table")
+class User (
+    @PrimaryKey(autoGenerate = true)
+    var id:Int,
+    var firstName:String,
+    var lastName:String,
+    var age:Int,
+)
+
+
