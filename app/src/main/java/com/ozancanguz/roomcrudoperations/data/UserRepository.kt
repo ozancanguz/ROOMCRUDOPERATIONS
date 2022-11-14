64e9864a16442ac1336f76433079292579fd05dc
Ozan <ozancangu24@gmail.com>
2022-11-14 10:16:02 +0300

    -user repository class coded

---
+++ app/src/main/java/com/ozancanguz/roomcrudoperations/data/UserRepository.kt
@@ -0,0 +1,18 @@
+package com.ozancanguz.roomcrudoperations.data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData:LiveData<List<User>> =userDao.readAllData()
+
+
+    suspend fun addUser(user:User){
+        userDao.insert(user)
+     }
+
+
+
+
+
+}
