b169e9b08671870f08f68919e868afb1b7193e13
Ozan <ozancangu24@gmail.com>
2022-11-14 10:26:05 +0300

    -user viewModel Coded

--- app/src/main/java/com/ozancanguz/roomcrudoperations/data/UserRepository.kt
+++ app/src/main/java/com/ozancanguz/roomcrudoperations/data/UserRepository.kt
@@ -1,10 +1,13 @@
 package com.ozancanguz.roomcrudoperations.data
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 class UserRepository(private val userDao: UserDao) {
 
     val readAllData:LiveData<List<User>> =userDao.readAllData()
+
+
 
 
     suspend fun addUser(user:User){
