5183ca237adff258c0849aa9934691944cb15d9a
Ozan <ozancangu24@gmail.com>
2022-11-14 10:32:26 +0300

    -navigation built -fragment list created -fragment add created

--- app/src/main/java/com/ozancanguz/roomcrudoperations/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/roomcrudoperations/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ozancanguz.roomcrudoperations
+package com.ozancanguz.roomcrudoperations.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ozancanguz.roomcrudoperations.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
